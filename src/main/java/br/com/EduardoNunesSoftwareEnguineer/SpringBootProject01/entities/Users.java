82550ed265f2c9063b1d95dec271ac28e3bd9a33
EduardoNunexx <eduardonunes@alu.ufc.br>
2024-03-24 01:11:40 -0300

    Create users

---
+++ src/main/java/br/com/EduardoNunesSoftwareEnguineer/SpringBootProject01/entities/Users.java
@@ -0,0 +1,75 @@
+package br.com.EduardoNunesSoftwareEnguineer.SpringBootProject01.entities;
+
+import java.util.Objects;
+
+public class Users {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    private Users(){
+    }
+
+    public Users(Long id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return Objects.equals(id, users.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+    
+}
